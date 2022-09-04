c2082f8277442edea9ab568475350ac29d85d5be
Ziphil <yfe23457@nifty.com>
2022-09-04 13:42:26 +0900

    レベル表示を1から始めるよう変更

--- source/entity/status-pane/status-pane.ts
+++ source/entity/status-pane/status-pane.ts
@@ -30,7 +30,7 @@ export class StatusPane extends Actor {
 
   public override onInitialize(engine: Engine): void {
     const scoreStatusItem = new StatusItem({x: 0, y: 10, spriteIndex: 0, getNumber: () => this.status.score});
-    const levelStatusItem = new StatusItem({x: 0, y: 40, spriteIndex: 1, getNumber: () => this.status.level});
+    const levelStatusItem = new StatusItem({x: 0, y: 40, spriteIndex: 1, getNumber: () => this.status.level + 1});
     const lifeStatusItem = new StatusItem({x: 0, y: 64, spriteIndex: 2, getNumber: () => this.status.life});
     const hitCountStatusItem = new StatusItem({x: 0, y: 94, spriteIndex: 3, getNumber: () => this.status.hitCount});
     const killCountStatusItem = new StatusItem({x: 0, y: 118, spriteIndex: 4, getNumber: () => this.status.killCount});
