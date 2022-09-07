67a3ea3a6c990483f4a1538f09f918d6c5382c38
Ziphil <yfe23457@nifty.com>
2022-09-07 22:56:55 +0900

    リファクタリング

--- source/entity/status-pane/status-item.ts
+++ source/entity/status-pane/status-item.ts
@@ -4,22 +4,15 @@ import {
   Actor,
   CollisionType,
   Engine,
+  Sprite,
+  Text,
   vec
 } from "excalibur";
 import {
   ASSETS,
+  SPRITE_FONTS,
   SPRITE_SHEETS
 } from "/source/core/asset";
-import {
-  FIELD_PROPS,
-  SCREEN_PROPS
-} from "/source/core/constant";
-import {
-  Image
-} from "/source/entity/image";
-import {
-  StringLabel
-} from "/source/entity/string-label";
 
 
 export type StatausItemConfigs = {
@@ -36,7 +29,8 @@ export class StatusItem extends Actor {
   private readonly spriteIndex: number;
   private readonly decimalLength?: number;
   private readonly getNumber: () => number;
-  private label!: StringLabel;
+  private frameSprite!: Sprite;
+  private text!: Text;
 
   public constructor({x, y, ...configs}: StatausItemConfigs) {
     super({
@@ -47,27 +41,41 @@ export class StatusItem extends Actor {
     this.spriteIndex = configs.spriteIndex;
     this.decimalLength = configs.decimalLength;
     this.getNumber = configs.getNumber;
-    this.graphics.use(ASSETS.statusFrame.toSprite());
   }
 
   public override onInitialize(engine: Engine): void {
-    this.addChildren();
+    this.initializeGraphics();
   }
 
   public override onPostUpdate(engine: Engine, delta: number): void {
     this.updateNumber();
   }
 
-  private addChildren(): void {
-    const label = new StringLabel({x: SCREEN_PROPS.width - FIELD_PROPS.width - 24, y: 8, decimalLength: this.decimalLength});
-    const nameImage = new Image({x: 8, y: 2, anchor: vec(0, 0), graphic: SPRITE_SHEETS.statusName.sprites[this.spriteIndex]});
-    this.label = label;
-    this.addChild(label);
-    this.addChild(nameImage);
+  private initializeGraphics(): void {
+    const frameSprite = ASSETS.statusFrame.toSprite();
+    this.graphics.use(frameSprite);
+    this.frameSprite = frameSprite;
+    const text = new Text({text: "", font: SPRITE_FONTS.char});
+    this.graphics.layers.create({name: "string", order: 1});
+    this.graphics.layers.get("string").use(text);
+    this.text = text;
+    const nameSprite = SPRITE_SHEETS.statusName.sprites[this.spriteIndex];
+    this.graphics.layers.create({name: "name", order: 1, offset: vec(8, 2)});
+    this.graphics.layers.get("name").use(nameSprite);
   }
 
   private updateNumber(): void {
-    this.label.string = this.getNumber();
+    this.text.text = formatString(this.getNumber(), this.decimalLength);
+    this.graphics.layers.get("string").offset = vec(this.frameSprite.width - this.text.width - 24, 4);
   }
 
+}
+
+
+function formatString(string: string | number, decimalLength?: number): string {
+  if (typeof string === "number") {
+    return string.toFixed(decimalLength ?? 0);
+  } else {
+    return string;
+  }
 }
